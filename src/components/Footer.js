5df9c629d45e7f6f417063ac6985b68fc6aa2e36
kostyrko <mkostyrko@gmail.com>
2020-10-23 10:32:07 +0200

    Create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer id="footer">
-        Footer
+    <footer id="footer" className="footer">
+      <div className="container">
+        <div className="copyright">Copyright by Coders Lab</div>
+        <div className="social">
+          <a className="facebook" href="http://facebook.com"> <div className="icon"></div> </a>
+          <a className="instagram" href="http://instagram.com"> <div className="icon"></div>  </a>
+        </div>
+      </div>
     </footer>
   );
 }
