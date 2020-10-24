11c8a4171b908dc89177f8c162aca1ec6320fd72
kostyrko <mkostyrko@gmail.com>
2020-10-24 22:19:47 +0200

    Set basic LogIn functionality

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,17 +1,82 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-// import { FirebaseContext } from '../components/Firebase';
 import * as ROUTES from "../constants/routers";
+import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
+import { withFirebase } from "../components/Firebase";
 
+
+
+const INITIAL_STATE = {
+  email: "",
+  password: "",
+  error: null,
+};
+
+const LogInFormBase = (props) => {
+  const [content, setContent] = useState({ ...INITIAL_STATE });
+
+  const onSubmit = (event) => {
+    const { email, password } = content;
+
+    props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        setContent({ ...INITIAL_STATE });
+        props.history.push(ROUTES.LANDING);
+      })
+      .catch((error) => {
+        setContent({ error });
+      });
+
+    event.preventDefault();
+  };
+
+  const onChange = (event) => {
+    setContent({ ...content, [event.target.name]: event.target.value });
+  };
+
+  const { email, password, error } = content;
+
+  const isInvalid = password === "" || email === "";
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="email"
+        value={email}
+        onChange={onChange}
+        type="text"
+        placeholder="Email Address"
+      />
+      <input
+        name="password"
+        value={password}
+        onChange={onChange}
+        type="password"
+        placeholder="Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Sign In
+      </button>
+
+      {error && <p>{error.message}</p>}
+    </form>
+  );
+};
+
+const LogInForm = compose(withRouter, withFirebase)(LogInFormBase);
 
 const LogIn = () => (
   <div>
-    <h1>SignUp</h1>
+    <h1>SignIn</h1>
+    <LogInForm />
     <div>
-        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+      Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
     </div>
   </div>
 );
 
 export default LogIn;
+
+// export { SignInForm };
